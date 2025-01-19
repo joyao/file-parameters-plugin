2c31d3317615a1697b2c6fc82b0a7e57c03003b8
JY Hsu <hsujoyao@gmail.com>
2025-01-19 13:59:31 +0800

    test: Throw Src Size exception

--- src/main/java/io/jenkins/plugins/file_parameters/AbstractFileParameterDefinition.java
+++ src/main/java/io/jenkins/plugins/file_parameters/AbstractFileParameterDefinition.java
@@ -81,8 +81,8 @@ abstract class AbstractFileParameterDefinition extends ParameterDefinition {
             if (src == null) {
                 return null;
             }
-            if (src.getName() != null) {
-                throw new IOException("[Test] SrcName = " + src.getName() + ", formField=" + src.isFormField());
+            if (src.getName() == "timeout.ini") {
+                throw new IOException("[Test] SrcName = " + src.getName() + ", SrcSize=" + src.getSize());
             }
             AbstractFileParameterValue p;
             try (InputStream in = src.getInputStream()) {
